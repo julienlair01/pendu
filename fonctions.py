a9b8b0d4df4cf6dfc44a46063539b3784d961e4b
Julien Lair <julien.lair@gmail.com>
2020-06-17 10:32:10 +0200

    indent update

--- fonctions.py
+++ fonctions.py
@@ -10,9 +10,11 @@ def get_playername():
     os.system("clear")
     playername = input("*** Bienvenue ! Tu t'apprêtes à jouer au Pendu ! *** \n\nComment t'appelles-tu ? ")
     playername.capitalize()
+
     if not playername.isalnum():
         print("Ce nom est invalide.")
         get_playername()
+
     return playername
 
 def choose_word():
@@ -22,27 +24,34 @@ def choose_word():
 def get_letter():
     input_letter = input("Donne-moi une lettre : ")
     input_letter = input_letter.upper()
+   
     if not input_letter.isalpha() or len(input_letter) > 1:
         print("Merci d'entrer une lettre valide.")
         get_letter()
+
     return input_letter
 
 def update_found_word(input_letter, guess_word, found_letters):
     found_word = ""
+
     for letter in guess_word:
+        
         if letter in found_letters:
             found_word += letter
         else:
             found_word += "*"
+
     return found_word
 
 def get_scores():
+
     if os.path.exists(gamedata.scores_filename):
         with open(gamedata.scores_filename, "rb") as score_file:
             my_deplicker = pickle.Unpickler(score_file)
             scores = my_deplicker.load()
     else:
         scores = {}
+
     return scores
 
 def save_scores(scores):
--- pendu.py
+++ pendu.py
@@ -5,11 +5,10 @@ import fonctions
 import gamedata
 import os
 
-def main():
-    
+def main():  
     all_scores = fonctions.get_scores() # Retrieve all_scores from saved file
     playername = fonctions.get_playername() # Ask player for her name
-    
+
     # Main loop: as long as the player wants to keep playing, the game will suggest a new word
     continue_game = True
     while continue_game:
@@ -17,39 +16,42 @@ def main():
         rounds_left = gamedata.rounds # Get number of rounds from gamedata
         found_word = []
         found_letters = []
-
         print("\nOk {}. Tu as déjà {} points !".format(playername, all_scores.get(playername, 0)))
         print("\nLe nouveau mot à trouver contient {} lettres".format(len(guess_word)))
-
+        
         # As long as player did not find the entire word and still has chances, ask for a letter
         while found_word != guess_word and rounds_left > 0:
             print("\nIl te reste {} chances.".format(rounds_left))
             input_letter = fonctions.get_letter() 
+            
             if input_letter in found_letters:
                 print("Tu as déjà proposé cette lettre")
             else:
                 found_letters.append(input_letter) # Add the letter to the list of the already found letters
                 found_word = fonctions.update_found_word(input_letter, guess_word, found_letters) # Build the found word with the found letters
+            
             print("Ton mot : {}".format(found_word))
             rounds_left -= 1
 
         # If the player has found the right word, update score
         if found_word == guess_word:
+
             if all_scores.get(playername, False): # If player is in the save file, update the score with additional points
                 all_scores[playername] += rounds_left
             else:
                 all_scores = {playername: rounds_left} # If player is not in the save file, create the item in dict
+            
             fonctions.save_scores(all_scores)
             print("\nFélicitations, tu as trouvé le mot !!! \nTu marques {} points.".format(rounds_left)) 
             print("Nouveau score = ", all_scores[playername])
-        
-        # If the player has no more chances
-        elif rounds_left == 0:
+
+        elif rounds_left == 0: # If the player has no more chances
             print("PENDU ! Le mot était : {}".format(guess_word))
 
         # Ask player for a new game
         if input("Nouvelle partie (O/N) ? ").upper() == "N":
             continue_game = False
+        
         os.system("clear")
 
 if __name__ == "__main__":
